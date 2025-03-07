7efe6d0f41c7315d884adcbaae4a8fb663a375db
bilguundalaim <bilguundalai3355@gmail.com>
2025-03-07 15:05:31 +0800

    add extract_bipartite_graph.py script and update .gitignore to include .txt files

---
+++ Data/e-mongolia/extract_bipartite_graph.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+# Reading all sheets from the excel file
+print("Reading all sheets from the excel file...")
+all_sheets = pd.read_excel("./request.xlsx", sheet_name=None)
+
+# Concatenating all sheets
+print("Concatenating all sheets...")
+df = pd.concat(all_sheets.values())
+
+# Dropping unnecessary columns
+print("Dropping unnecessary columns...")
+df = df[["userid", "service_id"]]
+
+# Grouping by use
+# rid and concatenating service_id
+print("Grouping by userid and concatenating service_id...")
+train_data = df.groupby("userid")["service_id"].apply(lambda x: " ".join(map(str, x))).reset_index()
+
+# Saving the data
+print("Saving the data...")
+train_data.to_csv("train.txt", sep=" ", index=False, header=False)
