9f50102ad6aabf30e8889f0872d0a3891a3dc529
bilguundalaim <bilguundalai3355@gmail.com>
2025-03-08 20:39:41 +0800

    add generate_kg.py script to create knowledge graph triples from service data

---
+++ Data/e-mongolia/generate_kg.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+# Reading data from excel file
+df_service = pd.read_excel('./emon.service.xlsx')
+
+# Creating a list to store the triples
+kg_triples = []
+
+# Iterating over the rows of the dataframe
+for index, row in df_service.iterrows():
+  # Extracting the service_id and agency_id
+  service_id = f"{row['_id']}"
+  agency_id = f"{row['govAgencyId']}"
+  
+  # Appending the triple to the list with the format: service_id provided_by agency_id
+  kg_triples.append(f"{service_id} provided_by {agency_id}")
+
+# Writing the triples to a file
+with open('kg_final.txt', 'w') as f:
+  for triple in kg_triples:
+    f.write(triple + "\n")
