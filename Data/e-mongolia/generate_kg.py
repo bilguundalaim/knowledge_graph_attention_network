4cdaf7537e8d1e4a957070aeb7c4d9100589d209
bilguundalaim <bilguundalai3355@gmail.com>
2025-03-08 20:42:10 +0800

    add progress print statements to generate_kg.py

--- Data/e-mongolia/generate_kg.py
+++ Data/e-mongolia/generate_kg.py
@@ -1,12 +1,14 @@
 import pandas as pd
 
 # Reading data from excel file
+print("Reading data from excel file...")
 df_service = pd.read_excel('./emon.service.xlsx')
 
 # Creating a list to store the triples
 kg_triples = []
 
 # Iterating over the rows of the dataframe
+print("Iterating over the rows of the dataframe...")
 for index, row in df_service.iterrows():
   # Extracting the service_id and agency_id
   service_id = f"{row['_id']}"
@@ -16,6 +18,7 @@ for index, row in df_service.iterrows():
   kg_triples.append(f"{service_id} provided_by {agency_id}")
 
 # Writing the triples to a file
+print("Writing the triples to a file...")
 with open('kg_final.txt', 'w') as f:
   for triple in kg_triples:
     f.write(triple + "\n")
