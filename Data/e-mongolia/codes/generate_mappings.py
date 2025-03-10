4b693e37eae651539ee8ef1966e765d9178e768c
bilguundalaim <bilguundalai3355@gmail.com>
2025-03-10 08:46:28 +0800

    add item list processing to generate_mappings.py for entity remapping

--- Data/e-mongolia/codes/generate_mappings.py
+++ Data/e-mongolia/codes/generate_mappings.py
@@ -1,6 +1,12 @@
 import pandas as pd
 
 kg_file = "../kg_final_raw.txt"
+item_list_file = "../item_list.txt"
+
+item_map = pd.read_csv(item_list_file, sep=" ", header=0, names=["org_id", "remap_id"], dtype=str)
+item_dict = dict(zip(item_map["org_id"], item_map["remap_id"])) 
+
+print("Max id in item_list: ", max(map(int, item_dict.values())))
 
 # Initialize sets to store entities and relations
 entities = set()
@@ -18,7 +24,17 @@ with open(kg_file, 'r') as f:
 
 # Assign unique ids to each entity and relation
 print("Assigning unique ids to entities and relations...")
-entity_mapping = {entity: idx for idx, entity in enumerate(sorted(entities))}
+
+entity_mapping = {}
+next_id = max(map(int, item_dict.values())) + 1
+
+for entity in sorted(entities):
+    if entity in item_dict:
+        entity_mapping[entity] = item_dict[entity]
+    else:
+        entity_mapping[entity] = str(next_id)
+        next_id += 1
+
 relation_mapping = {relation: idx for idx, relation in enumerate(sorted(relations))}
 
 # Write entity and relation mappings to files
